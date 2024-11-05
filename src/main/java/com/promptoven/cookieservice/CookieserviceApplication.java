166e85b8b6c39ee17799c1745d23b8d2d2e0aef5
jaehunLee-git <joz526@naver.com>
2024-11-05 16:11:23 +0900

    first commit

---
+++ src/main/java/com/promptoven/cookieservice/CookieserviceApplication.java
@@ -0,0 +1,13 @@
+package com.promptoven.cookieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promptoven/cookieservice/CookieserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptoven.cookieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
