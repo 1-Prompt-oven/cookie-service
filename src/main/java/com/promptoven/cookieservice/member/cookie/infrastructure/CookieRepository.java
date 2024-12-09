18bd1bbf6d5f320fb91eb245e484bc7d6871debc
jaehunLee-git <joz526@naver.com>
2024-12-09 14:31:31 +0900

    Feat : 배포 설정

--- src/main/java/com/promptoven/cookieservice/member/cookie/infrastructure/CookieRepository.java
+++ src/main/java/com/promptoven/cookieservice/member/cookie/infrastructure/CookieRepository.java
@@ -2,8 +2,6 @@ package com.promptoven.cookieservice.member.cookie.infrastructure;
 
 import com.promptoven.cookieservice.common.domain.Cookie;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CookieRepository extends MongoRepository<Cookie, String> {
 }
